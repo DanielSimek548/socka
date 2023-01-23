bd567af2fb06f17f20ff572f9c77cac50bb1817e
DanielSimek548 <73018736+DanielSimek548@users.noreply.github.com>
2023-01-23 15:38:51 +0100

    Add files via upload

---
+++ SOCka/src/socka/SOCka.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package socka;
+
+/**
+ *
+ * @author simek
+ */
+public class SOCka {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Index().setVisible(true);
+    }
+    
+}
